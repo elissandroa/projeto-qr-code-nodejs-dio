c9ae6759906f0c9b890eb651887c6a82f7be383b
elissandroa <elissandroa@hotmail.com>
2024-09-20 17:59:36 -0300

    Password Generator

--- src/services/password/create.js
+++ src/services/password/create.js
@@ -1,8 +1,11 @@
 import chalk from "chalk";
+import handle from "./handle.js";
 
 
 async function createPassword () {
-    console.log(chalk.green("Password"));    
+    console.log(chalk.green("Password"));   
+    const password = await handle();
+    console.log(chalk.yellow.bold(password));
 }
 
 
---
+++ src/services/password/handle.js
@@ -0,0 +1,38 @@
+async function permitedCharacters() {
+    let permited = [];
+
+    if (process.env.UPPERCASE_LETTERS === "true") {
+        permited.push(... "ABCDEFGHIJKLMNOPQWRSTUVXYZ");
+    }
+
+    if (process.env.LOWERCASE_LETTERS === "true") {
+        permited.push(... "abcdefghijklmnoprstuvwxyz");
+    }
+
+    if (process.env.NUMBERS === "true") {
+        permited.push(... "0123456789");
+    }
+
+    if (process.env.SPECIAL_CHARACTERS === "true") {
+        permited.push(... "@#$%&*!()_-=+?")
+    }
+
+    return permited;
+}
+
+
+async function handle() {
+    let characters = [];
+    let password = "";
+    const passwordLenght = process.env.PASSWORD_LENGTH;
+    characters = await permitedCharacters();
+
+
+    for (let i = 0; i < passwordLenght; i++) {
+        const index = Math.floor(Math.random() * characters.length)
+        password += characters[index];
+        return password;
+    }
+}
+
+export default handle;
