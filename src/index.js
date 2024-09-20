9c41270306ff8b3229deaa2b0d6b1d54ad0cdc2f
elissandroa <elissandroa@hotmail.com>
2024-09-20 11:31:09 -0300

    Usage Chalk

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import prompt from 'prompt'
 import { mainPrompt } from './prompts/prompt-main.js'
+import chalk from 'chalk';
 
 (async function main() {
     prompt.get(mainPrompt, async (err, choose) => {
         if (choose.select == 1) {
-            console.log("Escolheu QrCode");
+            console.log(chalk.bgWhite.black.bold("Escolheu QrCode!"));
         }
         if (choose.select == 2) {
             console.log("escolheu password");
--- src/prompts/prompt-main.js
+++ src/prompts/prompt-main.js
@@ -1,9 +1,11 @@
+import chalk from "chalk";
+
 export const mainPrompt = [
     {
         name: "select",
-        description: "Escolha a ferramenta (1 - QR Code ou (2 - Password",
+        description: chalk.yellow.bold("Escolha a ferramenta (1 - QR Code ou (2 - Password"),
         pattern: /^[1,2]+$/,
-        message: "Escolha apenas entre 1 e 2",
+        message: chalk.red.italic("Escolha apenas entre 1 e 2"),
         required: true,
     }
 ]
